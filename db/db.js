3231f904c29f8fd4c96bb9094384d1668243b62c
Rajat Gupta <grajat341@gmail.com>
2023-07-17 15:55:15 +0530

    Update db.js
    Updating Web Projects Name

--- db/db.js
+++ db/db.js
@@ -54,7 +54,7 @@ export const skills = [
 export const projects = {
   disclaimer:
     "*** All the projects I listed were worked by me personally",
-  webprojects: [
+  webProjects: [
     {
       projectName: "SKU Price Finder",
       image: "images/programmingdiaries.png",
